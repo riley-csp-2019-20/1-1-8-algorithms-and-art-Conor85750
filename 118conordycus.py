744bb44b93dd00dc4a312982331c3f8f55dc1a6d
Conor85750 <55500804+Conor85750@users.noreply.github.com>
2019-10-14 11:28:39 -0400

    finish dragon

--- 118conordycus.py
+++ 118conordycus.py
@@ -5,7 +5,7 @@ dragon = trtl.Turtle()
 fire = trtl.Turtle()
 
 fire_list = ["red","orange","yellow"]
-
+#makes the body
 dragon.speed(5)
 dragon.penup()
 dragon.goto(-180,0)
@@ -15,7 +15,7 @@ dragon.right(45)
 dragon.pensize(5)
 dragon.pencolor("red")
 
-
+# makes the head
 dragon.begin_fill()
 dragon.fillcolor("red")
 dragon.circle(90,360,4)
@@ -31,7 +31,7 @@ dragon.left(75)
 dragon.circle(75,360,3)
 dragon.end_fill()
 
-
+#makes the eyes
 dragon.penup()
 dragon.goto(135,210)
 dragon.pendown()
@@ -44,7 +44,7 @@ dragon.goto(190,210)
 dragon.pendown()
 dragon.circle(20)
 dragon.end_fill()
-
+# makes the mouth and part of the eyes.
 dragon.begin_fill()
 dragon.fillcolor("white")
 dragon.penup()
@@ -62,13 +62,72 @@ dragon.pendown()
 dragon.circle(50,360,3)
 dragon.end_fill()
 
+# This code makes the claws
+dragon.penup() 
+dragon.goto(-45,-45)
+dragon.pendown()    
+dragon.pencolor("red")
+dragon.begin_fill()
+dragon.fillcolor("red")
+dragon.right(75)
+dragon.circle(45,360,4)
+dragon.penup() 
+dragon.goto(-45,-105)
+dragon.pendown()
+dragon.circle(45,360,4)
+dragon.goto(-35,-105)
+dragon.circle(45,360,3)
+dragon.end_fill()
+
+dragon.penup()
+dragon.goto(-135,-45)
+dragon.pendown()
+dragon.pencolor("red")
+dragon.begin_fill()
+dragon.fillcolor("red")
+dragon.circle(45,360,4)
+dragon.penup() 
+dragon.goto(-135,-105)
+dragon.pendown()
+dragon.circle(45,360,4)
+dragon.goto(-125,-105)
+dragon.circle(45,360,3)
+dragon.end_fill()
+
+# makes the spikes on the body and tale.
+dragon.penup()
+dragon.goto(-75,75)
+dragon.pendown()
+dragon.begin_fill()
+dragon.fillcolor("red")
+dragon.circle(45,360,3)
+dragon.penup()
+dragon.goto(-115,75)
+dragon.pendown()
+dragon.circle(45,360,3)
+dragon.penup()
+dragon.goto(-155,75)
+dragon.pendown()
+dragon.circle(45,360,3)
+dragon.penup()
+dragon.goto(-195,75)
+dragon.pendown()
+dragon.circle(45,360,3)
+dragon.penup()
+dragon.goto(-235,55)
+dragon.pendown()
+dragon.circle(45,360,3)
+dragon.end_fill()
+
+
+
 
 fire.penup()
 fire.goto(215,125)
 fire.pendown()
 fire.speed(0)
 
-
+# makes the fire
 fire_1 = 0
 fire_2 = 0
 while fire_1 < 25:
@@ -80,17 +139,13 @@ while fire_1 < 25:
 fire.penup()
 fire.goto(215,145)
 fire.pendown()
+
 while fire_2 < 25:
     fire.pencolor(rdn.choice(fire_list))
     fire.pensize(5)
     fire.circle(25)
     fire.forward(10)
-    fire_1 += 1
-    
-    
-
-   
-
+    fire_2 += 1
 
 
 
